4e8c6c9b0710936216f8a52a6a5a426e8c84b6c7
smatas <smatas@wyplay.com>
2015-03-17 13:32:11 +0100

    Fix symlinks issues.

--- project/urls.py
+++ project/urls.py
@@ -10,5 +10,9 @@ urlpatterns = patterns('',
     url(r'^$', include(core.urls)),
     # urls for admin interface.
     # url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
