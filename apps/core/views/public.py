332b786c68d30f5bec723ec8b804cb90c355724e
sullivanMatas <sullivan.matas@gmail.com>
2015-04-25 11:34:26 +0200

    Use require_safe for visit only pages.

--- apps/core/views/public.py
+++ apps/core/views/public.py
@@ -1,19 +1,19 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_safe
 
 def _(string):
     return string
 
 
-@require_GET
+@require_safe
 def home(request):
     template = loader.get_template('core/home.html')
     context = RequestContext(request)
     return HttpResponse(template.render(context))
 
 
-@require_GET
+@require_safe
 def features(request):
     template = loader.get_template('core/features.html')
     context = RequestContext(request)
