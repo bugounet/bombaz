84728cb0021e0a75805cf28259a5d80ac30df61e
sullivanMatas <sullivan.matas@gmail.com>
2015-04-25 11:33:46 +0200

    Use named urls for templates.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,11 +3,8 @@ import core.views.public as core_public_views
 import core.views.users as core_user_views
 urlpatterns = patterns('',
     # main website : game showcase logged out/unsubscribed visitors.
-    # ex: /
-    url(r'^$', core_public_views.home, name='home'),
-    url(r'^features', core_public_views.features, name='features'),
-    url(r'^login', core_user_views.login_form_view, name='login_form'),
-    url(r'^subscribe', core_user_views.signup_form_view, name='signup_form'),
+    url(r'^$', core_public_views.home, name='bombaz:core:home'),
+    url(r'^features', core_public_views.features, name='bombaz:core:features'),
+    url(r'^login', core_user_views.login_form_view, name='bombaz:core:login'),
+    url(r'^subscribe', core_user_views.signup_form_view, name='bombaz:core:signup'),
 )
-
-
