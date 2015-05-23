7a0661a417053d61e816235847353ebecc89efa7
sullivanMatas <sullivan.matas@gmail.com>
2015-05-23 12:32:58 +0200

    Rework urls

--- apps/api_v1/urls.py
+++ apps/api_v1/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 import lobby.views.api.v1_0 as lobby_api_v1
-import matchmaker as matchmaker
+import matchmaker.urls as matchmaker
 
 urlpatterns = patterns(
     '',
-    url(r'^/1.0/scores/[0-9]+$', lobby_api_v1.scores),
-    url(r'^/1.0/matchmaker$', include(matchmaker.urls))
+    url(r'^1.0/scores/[0-9]+$', lobby_api_v1.scores),
+    url(r'^1.0/matchmaker/', include(matchmaker))
 )
--- apps/lobby/urls.py
+++ apps/lobby/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import patterns, url
-import lobby.views.api.v1_0 as lobby_api_v1
+from django.conf.urls import patterns, include, url
+import api_v1.urls as api_routes
 import lobby.views.website as lobby_view
 
 urlpatterns = patterns(
     '',
-    # main website : game showcase logged out/unsubscribed visitors.
+    # main website : game showcase logged out/un subscribed visitors.
     # ex: /
     url(r'^$', lobby_view.index, name="index"),
     url(r'^templates/home$', lobby_view.home, name="home"),
     url(r'^templates/scores$', lobby_view.scores, name="scores"),
     url(r'^templates/store$', lobby_view.store, name="store"),
     url(r'^templates/preferences', lobby_view.preferences, name="preferences"),
-    url(r'^api/1.0/scores/[0-9]+$', lobby_api_v1.scores),
+    url(r'^api/', include(api_routes, namespace="api_v1")),
 )
+
